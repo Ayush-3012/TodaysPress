51f5a4dde7e7e99a753e1d958e69fa391059d843
Ayush Kumar <100586445+Ayush-3012@users.noreply.github.com>
2023-07-22 18:09:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // const API_KEY = "33780e04ab8b4142bf17417cda780f46";
-const url = `https://newsapi.org/v2/everything?q=`;
+const proxyUrl = "https://cors-anywhere.herokuapp.com/"
+const url = `${proxyUrl}https://newsapi.org/v2/everything?q=`;
 var i = Math.floor(Math.random() * 100);
 var navItem = document.querySelectorAll(".navItem");
 
